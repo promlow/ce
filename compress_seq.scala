4fd76bfbba5a94bb35d3fefaaf05373885492c02
promlow <paul@romlow.com>
2014-03-24 17:21:52 -0500

    gotcha

--- compress_seq.scala
+++ compress_seq.scala
@@ -6,19 +6,19 @@ object Main extends App {
   for (l <- lines) {
     val xs = for (x <- l.split(" ").toList) yield x.toInt    
     val ys = compressSequence(xs)
-    println(ys map( y => y._1.toString + " " + y._2.toString) mkString(" "))
+    val zs = for (y <- ys) yield y._1.toString.trim + " " + y._2.toString.trim
+    println (zs.mkString(" ").trim)
   }
 
   def compressSequence(xs: List[Int]): List[(Int, Int)] = {
     @tailrec
     def compressor(acc: Int, head: Int, xs: List[Int], zs: List[(Int, Int)]): List[(Int, Int)] = xs match {
-	case Nil => (acc, head) :: zs
-	case x :: Nil => if (head == x) zs :+ (acc + 1, head);
-			 else zs :+ (1, x)
+	case Nil => zs :+ (acc, head)
+	case x :: Nil => if (head == x) List((acc + 1, head))
+			 else zs :+ (acc, head) :+ (1, x)
 	case x :: xs  => if (head == x) compressor(acc + 1, x, xs, zs);
 		         else compressor(1, x, xs, zs :+ (acc, head))
       }
     compressor(1, xs.head, xs.tail, List())
   }
 }
-// 5 81 7 94 9 65 9 20 8 41 9 0 8 25 10 82 8 57 10 10 6 58 7 66 1 47 9 72 7 90 10 79 5 97
